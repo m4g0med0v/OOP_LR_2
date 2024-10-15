7bfe8141c1d304a06d18730fa9b63c7d7ac44b89
m4gomedov <m4gomedov@internet.ru>
2024-10-15 20:22:45 +0300

    Added examples from LR

---
+++ examples/calc_tests.py
@@ -0,0 +1,41 @@
+import unittest
+
+import calc
+
+
+class CalcBasicTests(unittest.TestCase):
+    @unittest.skip("Temporaly skip test_add")
+    def test_add(self):
+        self.assertEqual(calc.add(1, 2), 3)
+
+    def test_sub(self):
+        self.assertEqual(calc.sub(4, 2), 2)
+
+    def test_mul(self):
+        self.assertEqual(calc.mul(2, 5), 10)
+
+    def test_div(self):
+        self.assertEqual(calc.div(8, 4), 2)
+
+
+class CalcTest(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(calc.add(1, 2), 3)
+
+    def test_sub(self):
+        self.assertEqual(calc.sub(4, 2), 2)
+
+    def test_mul(self):
+        self.assertEqual(calc.mul(2, 5), 10)
+
+    def test_div(self):
+        self.assertEqual(calc.div(8, 4), 2)
+
+
+@unittest.skip("Skip CalcExTests")
+class CalcExTests(unittest.TestCase):
+    def test_sqrt(self):
+        self.assertEqual(calc.sqrt(4), 2)
+
+    def test_pow(self):
+        self.assertEqual(calc.pow(3, 3), 27)
---
+++ examples/test_runner.py
@@ -0,0 +1,9 @@
+import unittest
+import calc_tests
+
+calcTestSuite = unittest.TestSuite()
+calcTestSuite.addTest(unittest.makeSuite(calc_tests.CalcBasicTests))
+calcTestSuite.addTest(unittest.makeSuite(calc_tests.CalcExTests))
+
+runner = unittest.TextTestRunner(verbosity=2)
+runner.run(calcTestSuite)
